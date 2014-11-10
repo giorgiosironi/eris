023fb567e04658f15522373315b70d5075aa41e8
Giorgio Sironi <info@giorgiosironi.com>
2014-11-10 18:10:17 +0100

    Extracted genVector()

--- ListConcatenationTest.php
+++ ListConcatenationTest.php
@@ -4,13 +4,22 @@ class ListConcatenationTest extends \PHPUnit_Framework_TestCase
 {
     public function testLengthIsConserved()
     {
-        $first = [rand(), rand()];
-        $second = [rand(), rand(), rand()];
+        $first = call_user_func($this->genVector(function() { return rand(); }));
+        $second = call_user_func($this->genVector(function() { return rand(); }));
         $this->assertEquals(
             count($first) + count($second),
             count(array_merge($first, $second))
         );
     }
 
-    
+    private function genVector($elementGenerator)
+    {
+        return function() use ($elementGenerator) {
+            $vector = [];
+            for ($i = 0; $i < rand(); $i++) {
+                $vector[] = $elementGenerator();
+            }
+            return $vector;
+        };
+    } 
 }
