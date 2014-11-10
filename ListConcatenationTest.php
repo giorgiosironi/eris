c15152a03767cd53c1f534a542ec68b2d62503f9
Giorgio Sironi <info@giorgiosironi.com>
2014-11-10 18:19:30 +0100

    Extracted forAll().

--- ListConcatenationTest.php
+++ ListConcatenationTest.php
@@ -2,21 +2,46 @@
 
 class ListConcatenationTest extends \PHPUnit_Framework_TestCase
 {
+    private $iterations = 100;
+
     public function testLengthIsConserved()
     {
-        $first = call_user_func($this->genVector($this->genInt()));
-        $second = call_user_func($this->genVector($this->genInt()));
-        $this->assertEquals(
-            count($first) + count($second),
-            count(array_merge($first, $second))
-        );
+        $this->forAll([
+            'first' => $this->genVector($this->genInt()),
+            'second' => $this->genVector($this->genInt()),
+        ])
+            ->__invoke(function($first, $second) {
+                $this->assertEquals(
+                    count($first) + count($second),
+                    count(array_merge($first, $second))
+                );
+            });
+    }
+
+    private function forAll($generators)
+    {
+        $this->generators = $generators;
+        return $this;
+    }
+
+    public function __invoke($assertion)
+    {
+        for ($i = 0; $i < $this->iterations; $i++) {
+            $values = [];
+            foreach ($this->generators as $name => $generator) {
+                $values[] = $generator();
+            }
+            call_user_func_array(
+                $assertion, $values
+            );
+        }
     }
 
     private function genVector($elementGenerator)
     {
         return function() use ($elementGenerator) {
             $vector = [];
-            for ($i = 0; $i < rand(); $i++) {
+            for ($i = 0; $i < rand(1, 10); $i++) {
                 $vector[] = $elementGenerator();
             }
             return $vector;
