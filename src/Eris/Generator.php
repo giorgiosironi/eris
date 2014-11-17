765891fc6803af10d1cd79a9155859f826b3466d
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-11-17 22:49:58 +0100

    Removed completed TODO

--- src/Eris/Generator.php
+++ src/Eris/Generator.php
@@ -2,9 +2,6 @@
 namespace Eris;
 
 /**
- * TODO: rename to Generator once inside a Brioche\ namespace.
- * PHP already has a Generator class inside the root namespace which clashes with this.
- *
  * Generic interface for a type <T>.
  */
 interface Generator
