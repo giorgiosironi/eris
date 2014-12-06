9afff5f9fc65899257d10a8b7548000fe96f8f1b
Giorgio Sironi <info@giorgiosironi.com>
2014-12-06 10:31:49 +0100

    Adding end2end tests for When

--- test/Eris/ExampleEnd2EndTest.php
+++ test/Eris/ExampleEnd2EndTest.php
@@ -4,10 +4,28 @@ use SimpleXMLElement;
 
 class ExampleEnd2EndTest extends \PHPUnit_Framework_TestCase
 {
+    private $testsByName;
+    private $results;
+    private $returnCode;
+
     public function testTheSampleAndSampleShrinkTestShouldBePassing()
     {
         $this->runExample('SampleTest.php');
         $this->assertAllTestsArePassing();
+    }
+
+    public function testWhenTests()
+    {
+        $this->runExample('WhenTest.php');
+        $this->assertTestsAreFailing(2);
+        $this->assertRegexp(
+            "/should be less or equal to 100, but/",
+            (string) $this->theTest('testWhenFailingWillNaturallyHaveALowEvaluationRatioSoWeDontWantThatErrorToObscureTheTrueOne')->failure
+        );
+        $this->assertRegexp(
+            "/Evaluation ratio .* is under the threshold/",
+            (string) $this->theTest('testWhenWhichSkipsTooManyValues')->error
+        );
     }
 
     private function runExample($testFile)
@@ -17,13 +35,35 @@ class ExampleEnd2EndTest extends \PHPUnit_Framework_TestCase
         $logFile = tempnam(sys_get_temp_dir(), 'phpunit_log_');
         $phpunitCommand = "vendor/bin/phpunit --log-junit $logFile $samplesTestCase";
         exec($phpunitCommand, $output, $returnCode);
-        $this->assertSame(0, $returnCode);
+        $this->returnCode = $returnCode;
         $this->results = new SimpleXMLElement(file_get_contents($logFile));
+    }
+
+    private function theTest($name)
+    {
+        if ($this->testsByName === null) { 
+            $this->testsByName = [];
+            foreach ($this->results->testsuite->testcase as $testCase) {
+                $testName = (string) $testCase->attributes()['name'];
+                $this->testsByName[$testName] = $testCase;
+            }
+        }
+
+        return $this->testsByName[$name];
     }
 
     private function assertAllTestsArePassing()
     {
-        $this->assertEquals(0, (string) $this->results->testsuite->attributes()['failures']);
-        $this->assertEquals(0, (string) $this->results->testsuite->attributes()['errors']);
+        $this->assertTestsAreFailing(0);
+    }
+
+    private function assertTestsAreFailing($number)
+    {
+        $this->assertSame($number, $this->returnCode);
+        $this->assertEquals(
+            $number,
+            ((string) $this->results->testsuite->attributes()['failures'])
+            + ((string) $this->results->testsuite->attributes()['errors'])
+        );
     }
 }
