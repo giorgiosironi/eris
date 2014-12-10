2acc8a168d2977ed62fdca338e0a57213dc5e7c6
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-12-10 08:13:51 +0100

    Generator\Boolean

---
+++ examples/BooleanTest.php
@@ -0,0 +1,21 @@
+<?php
+use Eris\Generator;
+use Eris\TestTrait;
+
+class BooleanTest extends PHPUnit_Framework_TestCase
+{
+    use TestTrait;
+
+    public function testBooleanValueIsTrueOrFalse()
+    {
+        $this->forAll([
+            Generator\bool(),
+        ])
+            ->then(function($boolValue) {
+                $this->assertTrue(
+                    ($boolValue === true || $boolValue === false),
+                    "$boolValue is true or false"
+                );
+            });
+    }
+}
---
+++ src/Eris/Generator/Boolean.php
@@ -0,0 +1,33 @@
+<?php
+namespace Eris\Generator;
+use Eris\Generator;
+
+function bool()
+{
+    return new Boolean();
+}
+
+class Boolean implements Generator
+{
+    public function __construct()
+    {
+    }
+
+    public function __invoke()
+    {
+        $booleanValues = [true, false];
+        $randomIndex = rand(0, count($booleanValues) - 1);
+
+        return $booleanValues[$randomIndex];
+    }
+
+    public function shrink($element)
+    {
+        return false;
+    }
+
+    public function contains($element)
+    {
+        return is_bool($element);
+    }
+}
