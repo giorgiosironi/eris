190ca619b3b500da5cdcac8889ab9afe3f7e8f88
gabriele.lana <gabriele.lana@cleancode.it>
2014-12-12 17:53:16 +0100

    Use Generator\Tuple to implement Generator\Vector

--- src/Eris/Generator/Vector.php
+++ src/Eris/Generator/Vector.php
@@ -9,61 +9,27 @@ function vector($size, Generator $elementsGenerator)
 
 class Vector implements Generator
 {
-    private $size;
-    private $elementsGenerator;
+    private $generator;
 
-    public function __construct($size, Generator $elementsGenerator)
+    public function __construct($size, Generator $generator)
     {
-        $this->size = $size;
-        $this->elementsGenerator = $elementsGenerator;
+        $this->generator = new Tuple(
+            array_fill(0, $size, $generator)
+        );
     }
 
     public function __invoke()
     {
-        $generator = $this->elementsGenerator;
-        $sampleVector = [];
-        for ($i = 0; $i < $this->size; $i++) {
-            $sampleVector[] = $generator();
-        }
-
-        return $sampleVector;
+        return $this->generator->__invoke();
     }
 
     public function shrink($vector)
     {
-        $elementsToShrink = rand(1, (int)($this->size/2));
-        $attempts = 10;
-
-        while ($elementsToShrink > 0 && $attempts > 0) {
-            $elementToShrink = rand(0, $this->size - 1);
-
-            $shrinkedValue = $this->elementsGenerator->shrink($vector[$elementToShrink]);
-
-            if ($shrinkedValue === $vector[$elementToShrink]) {
-                $attempts--;
-                continue;
-            }
-            $elementsToShrink--;
-            $vector[$elementToShrink] = $shrinkedValue;
-        }
-
-        return $vector;
+        return $this->generator->shrink($vector);
     }
 
-    public function contains($generatedVector)
+    public function contains($vector)
     {
-        return (count($generatedVector) === $this->size)
-            && $this->containsElement($generatedVector);
-    }
-
-    private function containsElement($generatedVector)
-    {
-        foreach ($generatedVector as $elementInVector) {
-            if (!$this->elementsGenerator->contains($elementInVector)) {
-                return false;
-            }
-        }
-
-        return true;
+        return $this->generator->contains($vector);
     }
 }
