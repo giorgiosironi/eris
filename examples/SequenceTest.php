22570ad625a3c4b101affcfce5f607d104dc88e6
gabriele.lana <gabriele.lana@cleancode.it>
2014-12-14 16:37:32 +0100

    Example of Generator\Sequence usage

---
+++ examples/SequenceTest.php
@@ -0,0 +1,29 @@
+<?php
+use Eris\Generator;
+
+class SequenceTest extends PHPUnit_Framework_TestCase
+{
+    use Eris\TestTrait;
+
+    public function testArrayReversePreserveLength()
+    {
+        $this
+            ->forAll([
+                Generator\seq(Generator\nat(), 100),
+            ])
+            ->then(function($array) {
+                $this->assertEquals(count($array), count(array_reverse($array)));
+            });
+    }
+
+    public function testArrayReverse()
+    {
+        $this
+            ->forAll([
+                Generator\seq(Generator\nat(), 100),
+            ])
+            ->then(function($array) {
+                $this->assertEquals($array, array_reverse(array_reverse($array)));
+            });
+    }
+}
