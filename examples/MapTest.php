2ef7c8ae0cdee85048e65294cac9457933e9fb82
Giorgio Sironi <info@giorgiosironi.com>
2015-12-07 08:23:38 +0100

    Generator/map() motivating example

---
+++ examples/MapTest.php
@@ -0,0 +1,28 @@
+<?php
+use Eris\Generator;
+
+class MapTest extends PHPUnit_Framework_TestCase
+{
+    use Eris\TestTrait;
+
+    public function testApplyingAFunctionToGeneratedValues()
+    {
+        $this->forAll(
+            Generator\vector(
+                3,
+                Generator\map(
+                    function($n) { return $n * 2; },
+                    Generator\nat()
+                )
+            )
+        )
+            ->then(function($tripleOfEvenNumbers) {
+                foreach ($tripleOfEvenNumbers as $number) {
+                    $this->assertTrue(
+                        $number % 2 == 0,
+                        "The element of the vector $number is not even"
+                    );
+                }
+            });
+    }
+}
