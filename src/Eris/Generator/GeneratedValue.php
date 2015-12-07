17e1e0a52f7e9df9e02d2857aeb3727fb6eebea9
Giorgio Sironi <info@giorgiosironi.com>
2015-12-07 11:06:47 +0100

    GeneratedValue Value Object extension

--- src/Eris/Generator/GeneratedValue.php
+++ src/Eris/Generator/GeneratedValue.php
@@ -1,24 +1,50 @@
 <?php
 namespace Eris\Generator;
 
-class GeneratedValue
+use Eris\Generator\GeneratedValue;
+use InvalidArgumentException;
+
+final class GeneratedValue
 {
     private $value;
     private $input;
+    private $generatorName;
     
-    public static function fromValueAndInput($value, $input)
+    public static function fromValueAndInput($value, $input, $generatorName = null)
     {
-        return new self($value, $input);
+        if ($value instanceof self) {
+            throw new InvalidArgumentException("It looks like you are trying to build a GeneratedValue whose value is another GeneratedValue. This is almost always an error as values will be passed as-is to properties and GeneratedValue should be hidden from them.");
+        }
+        return new self($value, $input, $generatorName);
+    }
+
+    public static function fromJustValue($value, $generatorName = null)
+    {
+        return new self($value, $value, $generatorName);
     }
     
-    private function __construct($value, $input)
+    private function __construct($value, $input, $generatorName)
     {
+        if ($value instanceof self) {
+            throw new InvalidArgumentException("It looks like you are trying to build a GeneratedValue whose value is another GeneratedValue. This is almost always an error as values will be passed as-is to properties and GeneratedValue should be hidden from them.");
+        }
         $this->value = $value;
         $this->input = $input;
+        $this->generatorName = $generatorName;
+    }
+
+    public function input()
+    {
+        return $this->input;
     }
 
     public function unbox()
     {
         return $this->value;
+    }
+
+    public function __toString()
+    {
+        return var_export($this, true);
     }
 }
