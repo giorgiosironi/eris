034bd762f25f29cb25eed74a38d815c9f9d1abad
Giorgio Sironi <info@giorgiosironi.com>
2016-03-04 11:07:40 +0100

    Testing associativity of bind
    My eyes are bleeding

--- test/Eris/Generator/BindGeneratorTest.php
+++ test/Eris/Generator/BindGeneratorTest.php
@@ -42,4 +42,45 @@ class BindGeneratorTest extends \PHPUnit_Framework_TestCase
         }
         $this->assertLessThanOrEqual(5, $value->unbox());
     }
+
+    public function testAssociativeProperty()
+    {
+        $firstGenerator = new BindGenerator(
+            new BindGenerator(
+                new ChooseGenerator(0, 5),
+                function($n) {
+                    return new ChooseGenerator($n * 10, $n * 10 + 1);
+                }
+            ),
+            function($m) {
+                return new VectorGenerator($m, new IntegerGenerator());
+            }
+        );
+        $secondGenerator = new BindGenerator(
+            new ChooseGenerator(0, 5),
+            function($n) {
+                return new BindGenerator(
+                    new ChooseGenerator($n * 10, $n * 10 + 1),
+                    function($m) {
+                        return new VectorGenerator($m, new IntegerGenerator());
+                    }
+                );
+            }
+        );
+        for ($i = 0; $i < 100; $i++) {
+            $this->assertIsAnArrayOfX0OrX1Elements($firstGenerator->__invoke($this->size)->unbox());
+            $this->assertIsAnArrayOfX0OrX1Elements($secondGenerator->__invoke($this->size)->unbox());
+        }
+    }
+
+    private function assertIsAnArrayOfX0OrX1Elements(array $value)
+    {
+        $this->assertTrue(
+            in_array(
+                count($value) % 10,
+                [0, 1]
+            ),
+            "The array has " . count($value) . " elements"
+        );
+    }
 }
