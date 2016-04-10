2eb049c5ce4e07f36ffb9e4d2d412a32670a3d5a
Giorgio Sironi <info@giorgiosironi.com>
2016-04-10 11:37:53 +0200

    Cleaner indentation for SuchThatTest forAll()

--- examples/SuchThatTest.php
+++ examples/SuchThatTest.php
@@ -7,77 +7,82 @@ class SuchThatTest extends \PHPUnit_Framework_TestCase
 
     public function testSuchThatBuildsANewGeneratorFilteringTheInnerOne()
     {
-        $this->forAll(
-            Generator\vector(
-                5,
-                Generator\suchThat(
-                    function($n) {
-                        return $n > 42;
-                    },
-                    Generator\choose(0, 1000)
+        $this
+            ->forAll(
+                Generator\vector(
+                    5,
+                    Generator\suchThat(
+                        function($n) {
+                            return $n > 42;
+                        },
+                        Generator\choose(0, 1000)
+                    )
                 )
             )
-        )
             ->then($this->allNumbersAreBiggerThan(42));
     }
 
     public function testFilterSyntax()
     {
-        $this->forAll(
-            Generator\vector(
-                5,
-                Generator\filter(
-                    function($n) {
-                        return $n > 42;
-                    },
-                    Generator\choose(0, 1000)
+        $this
+            ->forAll(
+                Generator\vector(
+                    5,
+                    Generator\filter(
+                        function($n) {
+                            return $n > 42;
+                        },
+                        Generator\choose(0, 1000)
+                    )
                 )
             )
-        )
             ->then($this->allNumbersAreBiggerThan(42));
     }
 
     public function testSuchThatAcceptsPHPUnitConstraints()
     {
-        $this->forAll(
-            Generator\vector(
-                5,
-                Generator\suchThat(
-                    $this->isType('integer'),
-                    Generator\oneOf(
-                        Generator\choose(0, 1000),
-                        Generator\string()
+        $this
+            ->forAll(
+                Generator\vector(
+                    5,
+                    Generator\suchThat(
+                        $this->isType('integer'),
+                        Generator\oneOf(
+                            Generator\choose(0, 1000),
+                            Generator\string()
+                        )
                     )
                 )
             )
-        )
             ->then($this->allNumbersAreBiggerThan(42));
     }
 
 
     public function testSuchThatShrinkingRespectsTheCondition()
     {
-        $this->forAll(
-            Generator\suchThat(
-                function($n) {
-                    return $n > 42;
-                },
-                Generator\choose(0, 1000)
+        $this
+            ->forAll(
+                Generator\suchThat(
+                    function($n) {
+                        return $n > 42;
+                    },
+                    Generator\choose(0, 1000)
+                )
             )
-        )
             ->then($this->numberIsBiggerThan(100));
     }
 
     public function testSuchThatShrinkingRespectsTheConditionButTriesToSkipOverTheNotAllowedSet()
     {
-        $this->forAll(
-            Generator\suchThat(
-                function($n) {
-                    return $n <> 42;
-                },
-                Generator\choose(0, 1000)
+        $this
+            ->forAll(
+                Generator\suchThat(
+                    function($n) {
+                        return $n <> 42;
+                    },
+                    Generator\choose(0, 1000)
+                )
             )
-        )
             ->then($this->numberIsBiggerThan(100));
     }
 
