456d8a0058ddadfd8829545cfb2831eb34a20d3b
Giorgio Sironi <g.sironi@elifesciences.org>
2016-07-10 18:30:57 +0100

    Covering GeneratedValueOptions::cartesianProduct()

---
+++ test/Generator/GeneratedValueOptionsTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Eris\Generator;
+
+// TODO: complete *unit* test coverage
+class GeneratedValueOptionsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCartesianProductWithOtherValues()
+    {
+        $former = new GeneratedValueOptions([
+            GeneratedValue::fromJustValue('a'),
+            GeneratedValue::fromJustValue('b'),
+        ]);
+        $latter = new GeneratedValueOptions([
+            GeneratedValue::fromJustValue('1'),
+            GeneratedValue::fromJustValue('2'),
+            GeneratedValue::fromJustValue('3'),
+        ]);
+        $product = $former->cartesianProduct($latter, function($first, $second) { return $first . $second; });
+        $this->assertEquals(6, count($product));
+        foreach ($product as $value) {
+            $this->assertRegexp('/^[ab][123]$/', $value->unbox());
+        }
+    }
+}
+
