d8a4adbb3815dc9a451a247e7db20f850082d810
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-09-16 21:50:11 +0200

    Expose protected TestTrait methods in Facade

--- src/Facade.php
+++ src/Facade.php
@@ -3,11 +3,37 @@ namespace Eris;
 
 class Facade
 {
-    use TestTrait;
+    use TestTrait {
+        limitTo as traitLimitTo;
+        minimumEvaluationRatio as traitMinimumEvaluationRatio;
+        shrinkingTimeLimit as traitShrinkingTimeLimit;
+        withRand as traitWithRand;
+    }
 
     public function __construct()
     {
         $this->erisSetupBeforeClass();
         $this->erisSetup();
     }
+
+    public function limitTo($limit)
+    {
+        return $this->traitLimitTo($limit);
+    }
+
+    public function minimumEvaluationRatio($ratio)
+    {
+        return $this->traitMinimumEvaluationRatio($ratio);
+    }
+
+    public function shrinkingTimeLimit($shrinkingTimeLimit)
+    {
+        return $this->traitShrinkingTimeLimit($shrinkingTimeLimit);
+    }
+
+    public function withRand($randFunction)
+    {
+        return $this->traitWithRand($randFunction);
+    }
+
 }
