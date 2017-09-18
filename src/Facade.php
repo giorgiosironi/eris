2b8ffaa9cf01ab7981f288de5f4ef53cae3adacc
Bekh-Ivanov Aleksey <bekh6ex@users.noreply.github.com>
2017-09-18 14:19:25 +0200

    Simplify methods' exposion

--- src/Facade.php
+++ src/Facade.php
@@ -4,36 +4,15 @@ namespace Eris;
 class Facade
 {
     use TestTrait {
-        limitTo as traitLimitTo;
-        minimumEvaluationRatio as traitMinimumEvaluationRatio;
-        shrinkingTimeLimit as traitShrinkingTimeLimit;
-        withRand as traitWithRand;
-    }
+		limitTo as public;
+		minimumEvaluationRatio as public;
+		shrinkingTimeLimit as public;
+		withRand as public;
+	}
 
     public function __construct()
     {
         $this->erisSetupBeforeClass();
         $this->erisSetup();
     }
-
-    public function limitTo($limit)
-    {
-        return $this->traitLimitTo($limit);
-    }
-
-    public function minimumEvaluationRatio($ratio)
-    {
-        return $this->traitMinimumEvaluationRatio($ratio);
-    }
-
-    public function shrinkingTimeLimit($shrinkingTimeLimit)
-    {
-        return $this->traitShrinkingTimeLimit($shrinkingTimeLimit);
-    }
-
-    public function withRand($randFunction)
-    {
-        return $this->traitWithRand($randFunction);
-    }
-
 }
