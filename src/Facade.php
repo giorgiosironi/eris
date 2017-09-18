80ab9f545d421af1bf4bcaa09421e04698ca1e2a
Aleksey Bekh-Ivanov (WMDE) <aleksey.bekh-ivanov@wikimedia.de>
2017-09-18 14:22:27 +0200

    Fix codestyle

--- src/Facade.php
+++ src/Facade.php
@@ -4,11 +4,11 @@ namespace Eris;
 class Facade
 {
     use TestTrait {
-		limitTo as public;
-		minimumEvaluationRatio as public;
-		shrinkingTimeLimit as public;
-		withRand as public;
-	}
+        limitTo as public;
+        minimumEvaluationRatio as public;
+        shrinkingTimeLimit as public;
+        withRand as public;
+    }
 
     public function __construct()
     {
