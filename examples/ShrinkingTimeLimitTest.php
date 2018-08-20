67b0f1199cad1fe7e2fa87c3a571f87643dff96a
Björn Büttner <idrinth@users.noreply.github.com>
2018-08-20 16:54:38 +0200

    restoring previous test

--- examples/ShrinkingTimeLimitTest.php
+++ examples/ShrinkingTimeLimitTest.php
@@ -14,10 +14,28 @@ class ShrinkingTimeLimitTest extends PHPUnit_Framework_TestCase
 {
     use Eris\TestTrait;
 
+    public function testLengthPreservation()
+    {
+        $this
+            ->shrinkingTimeLimit(2)
+            ->forAll(
+                Generator\string(),
+                Generator\string()
+            )
+            ->then(function ($first, $second) {
+                $result = very_slow_concatenation($first, $second);
+                $this->assertEquals(
+                    strlen($first) + strlen($second),
+                    strlen($result),
+                    "Concatenating '$first' to '$second' gives '$result'" . PHP_EOL
+                );
+            });
+    }
+
     /**
      * @eris-shrink 2
      */
-    public function testLengthPreservation()
+    public function testLengthPreservationFromAnnotation()
     {
         $this
             ->forAll(
