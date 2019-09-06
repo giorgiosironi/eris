d27e532e8b71337afe095c91ea00e25817cb3bb2
Philip Hofstetter <phofstetter@sensational.ch>
2019-09-06 14:23:30 +0200

    fix for PHP 7.4 string index access
    see https://wiki.php.net/rfc/deprecate_curly_braces_array_access

--- src/Generator/SubsetGenerator.php
+++ src/Generator/SubsetGenerator.php
@@ -33,7 +33,7 @@ class SubsetGenerator implements Generator
         $binaryDescription = str_pad(decbin($subsetIndex), count($this->universe), "0", STR_PAD_LEFT);
         $subset = [];
         for ($i = 0; $i < strlen($binaryDescription); $i++) {
-            $elementPresent = $binaryDescription{$i};
+            $elementPresent = $binaryDescription[$i];
             if ($elementPresent == "1") {
                 $subset[] = $this->universe[$i];
             }
