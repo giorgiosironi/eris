7222900261d0a4d8d811e9edd47bc1b50c7ac974
Ilario Pierbattista <987038+ilario-pierbattista@users.noreply.github.com>
2021-04-10 19:10:20 +0200

    Expose TestTrait's methods in Facade (from #110) (#141)

--- src/Facade.php
+++ src/Facade.php
@@ -3,7 +3,12 @@ namespace Eris;
 
 class Facade
 {
-    use TestTrait;
+    use TestTrait {
+        limitTo as public;
+        minimumEvaluationRatio as public;
+        shrinkingTimeLimit as public;
+        withRand as public;
+    }
 
     public function __construct()
     {
